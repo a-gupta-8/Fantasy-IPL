5508529663b8d26a202ce223c4f6b4f1f8582c0a
a-gupta-8 <avibox9511051@gmail.com>
2026-02-22 21:52:30 -0800

    merged banner under single div to remove overlap lines

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -6,10 +6,11 @@ export default function Banner() {
         <>
             <div class="banner">
                 <img src={IPL_Logo} alt="IPL_Logo" class="banner-image" />
-            </div>
-            <div class="banner-text">
                 <h3 class="banner-h3-text">FANTASY LEAGUE 2026</h3>
             </div>
+            {/* <div class="banner-text">
+                <h3 class="banner-h3-text">FANTASY LEAGUE 2026</h3>
+            </div> */}
         </>
     );
 }
